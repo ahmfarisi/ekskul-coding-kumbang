d97d7e127c7ff3b53fa9315d40b3477b7acd5676
Ahmad Farisi <ahmfarisi@gmail.com>
2022-11-11 16:51:51 +0700

    New Project if millionaire

---
+++ 06_if_millionaire.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	char jawaban;
+	
+	cout<<"============================="<<endl;
+	cout<<"WHO WANTS TO BE A MILLIONAIRE"<<endl;
+	cout<<"============================="<<endl<<endl;
+	
+	cout<<"Pertanyaan Pertama"<<endl;
+	cout<<"------------------"<<endl;
+	cout<<"Di Indonesia, 10 November dikenal sebagai hari ..."<<endl;
+	cout<<"A. Hari Pahlawan    B. Hari Pendidikan Nasional   "<<endl;
+	cout<<"C. Hari Buruh       D. Hari Kemerdekaan           "<<endl<<endl;
+	cout<<"Jawaban (A/B/C/D) : ";cin>>jawaban;
+	cout<<endl;
+	
+	if(jawaban != 'A'){
+		cout<<"JAWABAN SALAH !";
+		_exit(0);
+	}
+	
+	cout<<"Pertanyaan Kedua"<<endl;
+	cout<<"------------------"<<endl;
+	cout<<"Di bawah ini yang merupakan Kota/Kabupaten di Provinsi"<<endl;
+	cout<<"Sumatera Selatan, kecuali ..."<<endl;
+	cout<<"A. Palembang        B. Lahat                      "<<endl;
+	cout<<"C. Jambi            D. Pagaralam                  "<<endl<<endl;
+	cout<<"Jawaban (A/B/C/D) : ";cin>>jawaban;
+	cout<<endl;
+	
+	if(jawaban != 'C'){
+		cout<<"JAWABAN SALAH !";
+		_exit(0);
+	}
+	
+	return 0;
+}
